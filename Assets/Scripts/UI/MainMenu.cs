56c94d0ed627c8f1d376ec5f7bb42e29537999dc
Joaqui Murillo <jnmurillo630@gmail.com>
2025-03-17 23:28:33 -0700

    comm

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
@@ -28,5 +28,19 @@ public class MainMenu : MonoBehaviour
             Application.Quit();  // Quits the built application
         #endif
 
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  // Ensure your player has the "Player" tag
+        {
+
+            // Re-enable the cursor
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            // Load the MainMenu2 scene
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
